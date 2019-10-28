49c34eacb50676605e0fe71c8472f09544c9c760
Darangi <small_musa@hotmail.com>
2019-10-28 18:27:32 +0100

    Added failing test

---
+++ spec/scss-spec.js
@@ -0,0 +1,17 @@
+const dedent = require('dedent')
+
+describe('Language sass', () => {
+  beforeEach(async () => {
+    atom.config.set('core.useTreeSitterParsers', false)
+    await atom.packages.activatePackage('language-sass')
+  })
+
+  fit('Should tokenize - as selector', async () => {
+    const editor = await atom.workspace.open("foo.scss")
+
+    editor.setText(dedent`@extend .foo-bar-baz;`)
+
+    expect(editor.scopeDescriptorForBufferPosition([0,12]).toString()).toBe('.source.css.scss .meta.at-rule.extend.scss .entity.other.attribute-name.class.css')
+
+  })
+})
